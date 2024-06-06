8e26b7bcf93d1da25bfa60a5f638456457442340
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-06-06 12:22:23 +0530

    Add files via upload

---
+++ 80. Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,17 @@
+// -------------------------------- Code -----------------------------------------
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+
+        for(int& ele: nums){
+            if(i == 0 || i == 1 || nums[i-2] != ele){
+                nums[i] = ele;
+                i++;
+            }
+        }
+
+        return i;
+    }
+};
