7c187305aef4653a0440ca229d371c9545baa6cc
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-06-10 08:30:30 +0530

    Add files via upload

---
+++ 380. Insert Delete GetRandom O(1).cpp
@@ -0,0 +1,54 @@
+// --------------------- Code ---------------------------------
+
+class RandomizedSet {
+    vector<int> vec;
+    unordered_map<int, int> mp;
+public:
+    RandomizedSet() {
+        ios_base::sync_with_stdio(0);
+        std::cin.tie(0);
+        std::cout.tie(0);
+    }
+    
+    bool insert(int val) {
+        if(mp.find(val) != mp.end()){
+            return false;
+        }
+
+        vec.push_back(val);
+        mp[val] = vec.size()-1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(mp.find(val) == mp.end()){
+            return false;
+        }
+
+        int idx = mp[val];
+        int lastElement = vec.back();
+        vec.back() = val;
+
+        vec[idx] = lastElement;
+        mp[lastElement] = idx;
+
+        vec.pop_back();
+        mp.erase(val);
+
+        return true;
+    }
+    
+    int getRandom() {
+        int n = vec.size();
+        int randomIndex = rand() % n;
+        return vec[randomIndex];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
