edc405f9b8f76c34cfc60da584abb5e537dd5505
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-07-04 20:47:53 +0530

    Add files via upload

---
+++ 2181. Merge Nodes in Between Zeros.cpp
@@ -0,0 +1,69 @@
+// -------------------------------- Approach - 1 ----------------------------------------
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* P1 = head->next;
+        ListNode* P2 = P1;
+
+        while(P2 != NULL){
+            int sum = 0;
+            while(P2 != NULL && P2->val != 0){
+                sum += P2->val;
+                P2 = P2->next;
+            }
+
+            P1->val = sum;
+            P2 = P2->next;
+            P1->next = P2;
+            P1 = P1->next;
+        }
+
+        return head->next;
+    }
+};
+
+// -------------------------------- Approach - 2 ----------------------------------------
+// Recursion leap of faith
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        head = head->next;
+        if(head == NULL){
+            return head;
+        }
+
+        ListNode* temp = head;
+        int sum = 0;
+        while(temp->val != 0){
+            sum += temp->val;
+            temp = temp->next;
+        }
+
+        head->val = sum;
+        head->next = mergeNodes(temp);
+
+        return head;
+    }
+};
