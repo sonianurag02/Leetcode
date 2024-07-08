dc863068eb851ed46861f379f82991e56205dbee
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-07-08 10:51:47 +0530

    Update 1823. Find the Winner of the Circular Game.cpp

--- 1823. Find the Winner of the Circular Game.cpp
+++ 1823. Find the Winner of the Circular Game.cpp
@@ -12,3 +12,25 @@ public:
         return solve(n, k) + 1;
     }
 };
+
+// --------------------------- Approach - 2 (Using Vector) ---------------------------------
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        vector<int> vec;
+        
+        for(int i = 1; i <= n; i++){
+            vec.push_back(i);
+        }
+
+        int i = 0;
+        while(n > 1){
+            i = (i + (k-1)) % n;
+            vec.erase(vec.begin() + i);
+            n--;
+        }
+
+        return vec[0];
+    }
+};
