13cddc1f5cd0da88395ed78085e4583e201db387
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-07-11 20:31:18 +0530

    Add files via upload

---
+++ 392. Is Subsequence.cpp
@@ -0,0 +1,33 @@
+// ---------------------------- Approach - 1 -------------------------------
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+
+        map<char, vector<int>> mp;
+
+        for(int i = 0; i < n; i++){
+            mp[t[i]].push_back(i);
+        }
+
+        int prev = -1;
+
+        for(int i = 0; i < m; i++){
+            char ch = s[i];
+
+            if(mp.find(ch) == mp.end()) return false;
+
+            vector<int> indices = mp[ch];
+
+            auto it = upper_bound(begin(indices), end(indices), prev);
+
+            if(it == indices.end()) return false;
+
+            prev = *it;
+        }
+
+        return true;
+    }
+};
