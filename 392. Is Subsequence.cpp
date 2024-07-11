8ae2ce8ffe573782ea7f1e8aa969ebc2d0649f0e
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-07-11 21:08:33 +0530

    Update 392. Is Subsequence.cpp

--- 392. Is Subsequence.cpp
+++ 392. Is Subsequence.cpp
@@ -31,3 +31,41 @@ public:
         return true;
     }
 };
+
+// ---------------------------- Approach - 2 -------------------------------
+
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        int n = s.length();
+
+        stack<int> openBracketIndex;
+        vector<int> door(n);
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == '('){
+                openBracketIndex.push(i);
+            }
+            else if(s[i] == ')'){
+                int j = openBracketIndex.top();
+                openBracketIndex.pop();
+                door[j] = i;
+                door[i] = j;
+            }
+        }
+
+        string result;
+        int flag = 1;
+        for(int i = 0; i < n; i += flag){
+            if(s[i] == '(' || s[i] == ')'){
+                i = door[i];
+                flag = -flag;
+            }
+            else{
+                result.push_back(s[i]);
+            }
+        }
+
+        return result;
+    }
+};
