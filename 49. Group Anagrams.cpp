fca6614161b5f2f49edc8b351191c55a6f2467fb
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-07-25 13:00:12 +0530

    Add files via upload

---
+++ 49. Group Anagrams.cpp
@@ -0,0 +1,74 @@
+// ---------------------------- Approach - 1 (Using Sorting) ---------------------
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+
+        vector<vector<string>> result;
+
+        unordered_map<string, vector<string>> mp;
+
+        for(int i = 0; i < n; i++){
+            string temp = strs[i];
+
+            sort(begin(temp), end(temp));
+
+            mp[temp].push_back(strs[i]);
+        }
+
+        for(auto& it: mp){
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
+
+// ---------------------------- Approach - 2 (W/O Using Sorting) ---------------------
+
+class Solution {
+public:
+    string generate(string& word){
+        int arr[26] = {0};
+
+        for(char& ch: word){
+            arr[ch - 'a']++;
+        }
+
+        string new_word = "";
+
+        for(int i = 0; i < 26; i++){
+            int freq = arr[i];
+
+            if(freq > 0){
+                new_word += string(freq, i + 'a');
+            }
+        }
+
+        return new_word;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        int n = strs.size();
+
+        vector<vector<string>> result;
+
+        unordered_map<string, vector<string>> mp;
+
+        for(int i = 0; i < n; i++){
+            string word = strs[i];
+
+            string new_word = generate(word);
+
+            mp[new_word].push_back(word);
+        }
+
+        for(auto& it: mp){
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
