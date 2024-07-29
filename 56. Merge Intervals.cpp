04baca98c079cd21ee2c44857f60b9c6096f4a02
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-07-29 18:44:21 +0530

    Add files via upload

---
+++ 56. Merge Intervals.cpp
@@ -0,0 +1,21 @@
+// --------------------------------- Approach - 1 ------------------------------------
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> res;
+
+        for(auto interval: intervals){
+            if(res.empty() || res.back()[1] < interval[0]){
+                res.push_back(interval);
+            }
+            else{
+                res.back()[1] = max(res.back()[1], interval[1]);
+            }
+        }
+        return res;
+    }
+};
