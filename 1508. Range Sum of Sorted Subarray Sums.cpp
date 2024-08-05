6bae11e446ee8baf9c36fd795e1f735e8ab4a61b
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-08-05 12:38:22 +0530

    Update 1508. Range Sum of Sorted Subarray Sums.cpp

--- 1508. Range Sum of Sorted Subarray Sums.cpp
+++ 1508. Range Sum of Sorted Subarray Sums.cpp
@@ -86,3 +86,43 @@ public:
         return (result + mod) % mod;
     }
 };
+
+// ----------------------------- Approach - 3 -----------------------------------
+// Using Priority Queue
+
+class Solution {
+public:
+    typedef pair<int, int> P;
+    int mod = 1e9 + 7;
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        priority_queue<P, vector<P>, greater<P>> pq;
+
+        for(int i = 0; i < n; i++){
+            pq.push({nums[i], i});
+        }
+
+        int result = 0;
+
+        for(int count = 1; count <= right; count++){
+            auto p = pq.top();
+            pq.pop();
+
+            int sum = p.first;
+            int idx = p.second;
+
+            if(count >= left){
+                result = (result + sum)  % mod;
+            }
+
+            int new_idx = idx+1;
+            P new_pair;
+            if(new_idx < n){
+                new_pair.first = sum + nums[new_idx];
+                new_pair.second = new_idx;
+                pq.push(new_pair);
+            }
+        }
+
+        return result;
+    }
+};
