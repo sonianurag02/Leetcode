6bf34506be927779e1cd19d765946c17a6598474
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-09-19 21:32:02 +0530

    Add files via upload

---
+++ 241. Different Ways to Add Parentheses.cpp
@@ -0,0 +1,37 @@
+// ------------------------- Approach - 1 -----------------------------------
+
+class Solution {
+public:
+    vector<int> solve(string s){
+        vector<int> result;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '+' || s[i] == '-' || s[i] == '*'){
+                vector<int> left_result = solve(s.substr(0, i));
+                vector<int> right_result = solve(s.substr(i+1));
+
+                for(int& x: left_result){
+                    for(int& y: right_result){
+                        if(s[i] == '+'){
+                            result.push_back(x+y);
+                        } else if(s[i] == '*'){
+                            result.push_back(x*y);
+                        } else if(s[i] == '-'){
+                            result.push_back(x-y);
+                        }
+                    }
+                }
+            }
+        }
+
+        if(result.empty()){
+            result.push_back(stoi(s));
+        }
+
+        return result;
+    }
+
+    vector<int> diffWaysToCompute(string s) {
+        return solve(s);
+    }
+};
