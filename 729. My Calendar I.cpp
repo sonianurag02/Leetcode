2d9677af39e392459094e25f5c1ebea29d024c6f
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-09-26 13:46:23 +0530

    Add files via upload

---
+++ 729. My Calendar I.cpp
@@ -0,0 +1,37 @@
+// ---------------------- Approach - 1 -------------------------
+// Using set
+
+class MyCalendar {
+public:
+    set<pair<int, int>> st;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        // Find the first event that starts after or at the same time of (start, end)
+        auto it = st.lower_bound({start, end});
+
+        // Check if the current event overlaps with the next event
+        if(it != st.end() && it->first < end){
+            return false;
+        }
+
+        // Check if the current event overlaps with the previous event
+        if(it != st.begin()){
+            auto prevIt = prev(it);
+            if(start < prevIt->second){
+                return false;
+            }
+        }
+
+        st.insert({start, end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
