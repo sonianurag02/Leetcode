ed0e0e40f3eda7763218919223314a116f423c05
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-09-28 16:18:09 +0530

    Add files via upload

---
+++ 641. Design Circular Deque.cpp
@@ -0,0 +1,80 @@
+// --------------------------- Approach - 1 --------------------------
+// Using vector
+
+class MyCircularDeque {
+public:
+    vector<int> que;
+    int front;
+    int rear;
+    int size;
+    int capacity;
+
+    MyCircularDeque(int k) {
+        que = vector<int>(k);
+        size = 0;
+        capacity = k;
+        front = 0;
+        rear = k-1;
+    }
+    
+    bool insertFront(int value) {
+        if(isFull()) return false;
+        front = (front - 1 + capacity) % capacity;
+        que[front] = value;
+        size++;
+        return true;
+    }
+    
+    bool insertLast(int value) {
+        if(isFull()) return false;
+        rear = (rear + 1) % capacity;
+        que[rear] = value;
+        size++;
+        return true;
+    }
+    
+    bool deleteFront() {
+        if(isEmpty()) return false;
+        front = (front + 1) % capacity;
+        size--;
+        return true; 
+    }
+    
+    bool deleteLast() {
+        if(isEmpty()) return false;
+        rear = (rear - 1 + capacity) % capacity;
+        size--;
+        return true;
+    }
+    
+    int getFront() {
+        if(isEmpty()) return -1;
+        return que[front];
+    }
+    
+    int getRear() {
+        if(isEmpty()) return -1;
+        return que[rear];
+    }
+    
+    bool isEmpty() {
+        return size == 0;
+    }
+    
+    bool isFull() {
+        return size == capacity;
+    }
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
+ */
