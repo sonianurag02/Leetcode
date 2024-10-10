7f8da6c818b89ce28252d128fe0b4b8ea6af1000
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-10-10 15:22:15 +0530

    Add files via upload

---
+++ 22. Generate Parentheses.cpp
@@ -0,0 +1,87 @@
+// ----------------------- Approach - 1 ----------------------------
+// Backtracking
+
+class Solution {
+public:
+    vector<string> result;
+
+    bool isValid(string& str){
+        int count = 0;
+
+        for(char& ch: str){
+            if(ch == '('){
+                count++;
+            } else {
+                count--;
+            }
+            if(count < 0){
+                return false;
+            }
+        }
+
+        return count == 0;
+    }
+
+    void solve(string& curr, int n){
+        if(curr.length() == 2*n){
+            if(isValid(curr)){
+                result.push_back(curr);
+            }
+            return;
+        }
+
+        curr.push_back('(');
+        solve(curr, n);
+        curr.pop_back();
+
+        curr.push_back(')');
+        solve(curr, n);
+        curr.pop_back();
+    }
+
+    vector<string> generateParenthesis(int n) {
+        string curr = "";
+
+        solve(curr, n);
+
+        return result;
+    }
+};
+
+// ----------------------- Approach - 2 ----------------------------
+// Better Approach
+
+class Solution {
+public:
+    vector<string> result;
+
+    void solve(string& curr, int n, int open, int close){
+        if(curr.length() == 2*n){
+            result.push_back(curr);
+            return;
+        }
+
+        if(open < n){
+            curr.push_back('(');
+            solve(curr, n, open + 1, close);
+            curr.pop_back();
+        }
+
+        if(close < open){
+            curr.push_back(')');
+            solve(curr, n, open, close + 1);
+            curr.pop_back();
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        string curr = "";
+
+        int open = 0;
+        int close = 0;
+
+        solve(curr, n, open, close);
+
+        return result;
+    }
+};
