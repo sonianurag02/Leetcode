291b9739acbb841ef05bc385e4cfe99453806992
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-10-14 15:52:09 +0530

    Add files via upload

---
+++ 2530. Maximal Score After Applying K Operations.cpp
@@ -0,0 +1,25 @@
+// ------------------------------------- Approach - 1 ---------------------------------
+// Using max heap
+// T.C.: O(n*klog(n))
+
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        long long sum = 0;
+
+        priority_queue<int> pq(begin(nums), end(nums));
+
+        while(k--){
+            int maxEl = pq.top();
+            pq.pop();
+
+            sum += maxEl;
+
+            maxEl = ceil(maxEl / 3.0);
+
+            pq.push(maxEl);
+        }
+
+        return sum;
+    }
+};
