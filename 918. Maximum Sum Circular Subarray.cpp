c1ad776217caf404a361f31600dca5b91d866fc5
Anurag Soni <59157051+sonianurag02@users.noreply.github.com>
2024-10-23 02:07:56 +0530

    Add files via upload

--- 918. Maximum Sum Circular Subarray.cpp
+++ 918. Maximum Sum Circular Subarray.cpp
@@ -1,3 +1,47 @@
 // ---------------------------- Approach - 1 -----------------------------
 // Using Kadane's Algorithm
 
+class Solution {
+public:
+    int kadanesMax(vector<int>& nums, int n){
+        int sum = nums[0];
+        int maxSum = nums[0];
+        
+        for(int i = 1; i < n; i++){
+            sum = max(sum + nums[i], nums[i]);
+            maxSum = max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+
+    int kadanesMin(vector<int>& nums, int n){
+        int sum = nums[0];
+        int minSum = nums[0];
+        
+        for(int i = 1; i < n; i++){
+            sum = min(sum + nums[i], nums[i]);
+            minSum = min(minSum, sum);
+        }
+
+        return minSum;
+    }
+
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int n = nums.size();
+
+        int sum = accumulate(begin(nums), end(nums), 0);
+
+        int minSum = kadanesMin(nums, n);
+
+        int maxSum = kadanesMax(nums, n);
+
+        int circularSum = sum - minSum;
+
+        if(maxSum > 0){
+            return max(maxSum, circularSum);
+        }
+
+        return maxSum;
+    }
+};
